3be8c2c822a731623c8f4b063ea7b1fe4270436e
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-10-24 17:59:43 +0300

    completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+def countdown number
+    while number > 0
+      p  "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep integer
+    while integer >= 1
+       p "#{integer}SECOND(S)!"
+        sleep 1
+           integer -= 1
+           "HAPPY NEW YEAR!"
+       end
+       
+end
+
+
